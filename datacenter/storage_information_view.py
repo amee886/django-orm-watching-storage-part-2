782063eb6e411c80e73cc78ca44e763efd2b3339
amee886 <nsecex@gmail.com>
2026-04-23 22:13:05 +0300

    добавил код

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,19 +1,26 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from django.utils.timezone import localtime,now
+import django
+from datetime import datetime, timedelta
+from datacenter.models import format_duration
+from datacenter.models import get_duration 
 
 
 def storage_information_view(request):
-    # Программируем здесь
+    visits = Visit.objects.select_related('passcard').filter(leaved_at__isnull=True)
+    result = []
+    for visit in visits:
+        duration = get_duration(visit)
+        result.append({
+            'who_entered': visit.passcard.owner_name,
+            'entered_at': localtime(visit.entered_at),
+            'duration': format_duration(duration),
+        })
 
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': result
     }
     return render(request, 'storage_information.html', context)
+
