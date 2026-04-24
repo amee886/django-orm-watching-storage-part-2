0202b5d7bcdb572d63de93da86e39b130545922b
amee886 <nsecex@gmail.com>
2026-04-24 19:53:56 +0300

    убрал ненужный код

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 
 
 def active_passcards_view(request):
-    all_passcards = Passcard.objects.all()
     published_posts=Passcard.objects.filter(is_active=True)
     context = {
         'active_passcards': published_posts, 
